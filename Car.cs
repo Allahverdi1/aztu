306483ffbadd2dbcd911e0b01b49a1072e347f98
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-03-13 23:15:21 +0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    internal class Car:Vehicle
+    {
+        public string BrandName;
+        public string Model;
+    }
+}
