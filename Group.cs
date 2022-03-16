68def02622ad6a7f089fdce410797c9db43397b0
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-03-17 00:50:14 +0400

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5
+{
+    internal class Group
+    {
+        private string _no;
+        private int _studentLimit;
+
+        public int StudentLimit
+        {
+            get => this._studentLimit;
+            set
+            {
+                if (value > 0)
+                    this._studentLimit = value;
+            }
+        }
+        public string No
+        {
+            get => this._no;
+            set
+            {
+                if (CheckNo(value))
+                    this._no = value;
+            }
+        }
+        public bool HasDigit(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 2; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        public bool HasUpper(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i]))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
+        
+        
+
+        
+    
